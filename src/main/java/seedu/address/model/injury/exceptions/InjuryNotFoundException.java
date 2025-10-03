fd3e5ccd69b94fdeed8317e9efc62b510fc6fec4
yuanz03 <chinyuantian03@gmail.com>
2025-10-03 16:07:17 +0800

    feat: introduce injury-related exceptions

---
+++ src/main/java/seedu/address/model/injury/exceptions/InjuryNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.address.model.injury.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified injury.
+ */
+public class InjuryNotFoundException extends RuntimeException {}
