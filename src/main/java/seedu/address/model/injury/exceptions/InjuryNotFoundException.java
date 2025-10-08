6e34ffb81aabee2093df410d008e6ed016892f70
yuanz03 <chinyuantian03@gmail.com>
2025-10-08 15:58:07 +0800

    chore: remove unnecessary files

--- src/main/java/seedu/address/model/injury/exceptions/InjuryNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package seedu.address.model.injury.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified injury.
- */
-public class InjuryNotFoundException extends RuntimeException {}
