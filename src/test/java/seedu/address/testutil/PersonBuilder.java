b898d0f541fcae303926669f50144bad12646abf
yuanz03 <chinyuantian03@gmail.com>
2025-10-09 16:51:33 +0800

    test: add injury to storage unit tests

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -22,7 +22,7 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
-    public static final String DEFAULT_INJURY = "FIT";
+    public static final String DEFAULT_INJURY = "ACL";
     public static final String DEFAULT_TEAM = "U12";
 
     private Name name;
