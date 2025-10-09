65c13ac337548dc29c91206b864cfce13e6f1b45
gabrieltang515 <gabrieltang515@gmail.com>
2025-10-09 20:15:25 +0800

    Import position in PersonBuilder.java

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -8,6 +8,7 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.position.Position;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.team.Team;
 import seedu.address.model.util.SampleDataUtil;
