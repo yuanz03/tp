0f22487b58a3ff83f47a50dd6f4826c2f43fa828
jovnc <95868357+jovnc@users.noreply.github.com>
2025-10-11 11:50:18 +0800

    test: Add unit tests for AssignTeamCommand and its parser

--- src/test/java/seedu/address/testutil/TeamUtil.java
+++ src/test/java/seedu/address/testutil/TeamUtil.java
@@ -1,8 +1,12 @@
 package seedu.address.testutil;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PLAYER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TEAM;
 
 import seedu.address.logic.commands.AddTeamCommand;
+import seedu.address.logic.commands.AssignTeamCommand;
+import seedu.address.model.person.Name;
 import seedu.address.model.team.Team;
 
 /**
@@ -14,6 +18,13 @@ public class TeamUtil {
      */
     public static String getAddCommand(Team team) {
         return AddTeamCommand.COMMAND_WORD + " " + getTeamDetails(team);
+    }
+
+    /**
+     * Returns an assign team command string for assigning the {@code team} to a {@code playerName}.
+     */
+    public static String getAssignTeamCommand(Name playerName, Team team) {
+        return AssignTeamCommand.COMMAND_WORD + " " + PREFIX_PLAYER + playerName + " " + PREFIX_TEAM + team.getName();
     }
 
     /**
