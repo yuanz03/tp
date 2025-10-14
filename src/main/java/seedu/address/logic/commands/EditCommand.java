22f13d2451b3fb80467682c75eb6374415f87d78
yuanz03 <chinyuantian03@gmail.com>
2025-10-14 20:21:34 +0800

    fix: reject EditCommand with non-existent team

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -61,6 +61,8 @@ public class EditCommand extends Command {
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
+    public static final String MESSAGE_TEAM_NOT_FOUND = "The team '%1$s' does not exist. "
+            + "Please create the team first using the 'addteam' command.";
 
     private final Name personNameToEdit;
     private final EditPersonDescriptor editPersonDescriptor;
@@ -130,6 +132,10 @@ public class EditCommand extends Command {
 
         if (!personToEdit.isSamePerson(editedPerson) && model.hasPerson(editedPerson)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+        if (!model.hasTeam(editedPerson.getTeam())) {
+            throw new CommandException(String.format(MESSAGE_TEAM_NOT_FOUND, editedPerson.getTeam().getName()));
         }
 
         model.setPerson(personToEdit, editedPerson);
