fbf406e2455de8f421a0481f2344ba62553710a2
yuanz03 <chinyuantian03@gmail.com>
2025-10-18 23:41:51 +0800

    feat: Support multiple injuries in sample data

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -15,7 +15,6 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
             "Multiple values specified for the following single-valued field(s): ";
@@ -63,7 +62,7 @@ public class Messages {
                 .append("; Team: ")
                 .append(person.getTeam())
                 .append("; Injury Status: ")
-                .append(person.getInjury())
+                .append(person.getInjuries())
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
         return builder.toString();
