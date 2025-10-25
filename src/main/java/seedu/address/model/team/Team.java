ec5e97e597f89d1db1a4c5a9d5abb9407ae9784c
jovnc <95868357+jovnc@users.noreply.github.com>
2025-10-25 10:33:37 +0800

    fix: Fix missing space in team name

--- src/main/java/seedu/address/model/team/Team.java
+++ src/main/java/seedu/address/model/team/Team.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class Team {
 
     public static final String MESSAGE_CONSTRAINTS = "Team names should only contain alphanumeric characters"
-            + "and spaces, and it should not be blank";
+            + " and spaces, and it should not be blank";
     public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     // Identity fields
